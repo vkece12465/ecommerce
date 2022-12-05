35c5c1188cb245144ac57c8508fc83ec3d31dfd2
VINOD KUMAR M <34544927+vkece12465@users.noreply.github.com>
2022-12-05 18:33:13 +0000

    Product schema model added

--- models/product.schema.js
+++ models/product.schema.js
@@ -4,11 +4,42 @@ const productSchema = new mongoose.Schema(
     {
         name: {
             type: String,
-            maxlength: [40, "Maximum 40 charactors are allowed"],
+            maxlength: [60, "Product name maximum 60 charactors are allowed"],
             required: [true, "Product name is required"],
             trim: true
 
+        },
+        
+        price: {
+            type: Number,
+            required: [true, "Please provide product price"],
+            maxlength: [5, "Price should not go 5 digits"]
+        },
+        description: {
+            type: String,
+            // wysiwyg text editor for feature usages
+        },
+        photos: [
+            {
+                secure_url: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        stocks: {
+            type: Number,
+            default: 0
+        },
+        sold: {
+            type: Number,
+            default: 0
+        },
+        collectionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Collection"
         }
+        
     },
 
     // Timestamps
