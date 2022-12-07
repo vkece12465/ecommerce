72615256ee119072e345586e56b591daff648435
VINOD KUMAR M <34544927+vkece12465@users.noreply.github.com>
2022-12-07 18:03:57 +0000

    Mail transporter created with Nodemailer

--- config/index.js
+++ config/index.js
@@ -6,7 +6,14 @@ const dotenv = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "2d",
     MONGO_URL: process.env.MONGO_URL,
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    SMTP_MAIL_HOST: process.env.SMTP_MAIL_HOST,
+    SMTP_MAIL_PORT: process.env.SMTP_MAIL_PORT,
+    SMTP_MAIL_USERNAME: process.env.SMTP_MAIL_USERNAME,
+    SMTP_MAIL_PASSWORD: process.env.SMTP_MAIL_PASSWORD,
+
+
+
 }
 
 export default config;
---
+++ config/mailTransport.config.js
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer';
+import config from './index'
+
+const mailTransport = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
