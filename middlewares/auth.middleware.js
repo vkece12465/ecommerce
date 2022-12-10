db4638a069c63e6d1ca3782d6d91af6ed2086e56
VINOD KUMAR M <34544927+vkece12465@users.noreply.github.com>
2022-12-10 18:50:57 +0000

    Middleware created for logged in

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import User from './../models/user.schema';
+import JWT from 'jsonwebtoken';
+import asyncHandler from '../services/asyncHandler';
+import customError from '../utils/customError';
+import config from '../config';
+
+export const isLoggedIn = asyncHandler(async (req, res, next) => {
+    let token;
+    if(req.cookies.token || (req.headers.authorization && req.headers.authorization.startsWith('bearer'))
+    ) {
+        // split(" ")[0]--> 0 place token [1] place valus
+    token = req.cookies.token || req.headers.authorization.split(" ")[1]
+    }
+    if(!token){
+        throw new customError("Access denied", 401)
+    }
+
+    // try catch block
+    try {
+        const decodedPayload = JWT.verify(token, config.JWT_SECRET)
+        
+        // Find the user, role, email
+        req.user = await User.findById(decodedPayload._id, "name email role")
+        next()
+    } catch (error) {
+        throw new customError("Access denied", 401)
+    }
+    
+})
